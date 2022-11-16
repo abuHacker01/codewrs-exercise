51f74bc67dcdc995a50551a7f0e7c865eb8a6992
abuhacker01 <abuhacker012@gmail.com>
2022-11-16 14:35:59 +0500

    codewars exercises

---
+++ index.js
@@ -0,0 +1,40 @@
+// let a = 1;
+// let b = 1;
+
+// let c = ++a; // 2
+// let d = b++; // 1
+
+// console.log(c)......................// 3
+// console.log(d)......................// 1 , ikki bo'lib kegingi konsolimizda chiqadi
+
+// let a = 2;
+// let x = 1 + (a *= 2);...............// 5
+
+// let x = "" + 1 + 0;.................// 10
+// let c = "" - 1 + 0;.................// 10
+
+// let v = true + false;...............// 1
+
+// let b = 6 / "3";....................// 2
+// let n = "2" * "3";..................// 6
+// let m = 4 + 5 * "px";...............// NaN
+// let x = "$" + 4 + 5;................// $45
+// let z = "4" - 2;....................// NaN
+// let x = "4px" - 2;..................// 2
+
+// let c = "   -9   " + 5;.............//    -9   5. Stringda chiqadi .
+// let v = "   -9   " - 5;.............// -14
+
+// let x = null + 1....................// 1
+// let x = undefined + 1;..............// NanN
+// let a = " \t \n" - 2;...............// -2
+
+// console.log(5 > 4);.................//true
+// console.log("apple" > "pineapple");.//false
+// console.log("2" > "12");
+
+// console.log(undefined == null);.....// true
+// console.log(undefined === null);....// false
+
+// null == "\n0\n";....................// true
+// null === +"\n0\n";..................// false
